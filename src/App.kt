4904cc95260899a542930ad005d21a7a523dee4f
Bruno <ichigoplayer18@gmail.com>
2024-08-07 13:55:15 +0200

    new changes

---
+++ src/App.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var lista : List<Int> = listOf(2,4,5);
+    var sett : Set<String> = setOf("oko","dfikdf","d");
+    var test=10;
+    println("Hello World $test + test = ${test + 1}  + ${lista} + ${sett}")
+}
